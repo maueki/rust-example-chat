a1f63860f558b99105c936943c650ee38c23107e
Ueki Masaru <masaru.ueki@itage.co.jp>
2017-05-26 18:00:35 +0900

    WebSocketの通信開始処理
    PollOpt::oneshot()使うとpanicするので、コメントアウト

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 extern crate mio;
+extern crate http_muncher;
 
 use std::net::SocketAddr;
 use mio::*;
@@ -6,9 +7,48 @@ use mio::net::*;
 use std::collections::HashMap;
 use std::io::Read;
 
+use http_muncher::{Parser, ParserHandler};
+
+struct HttpParser;
+impl ParserHandler for HttpParser {}
+
+struct WebSocketClient {
+    socket: TcpStream,
+    http_parser: Parser
+}
+
+impl WebSocketClient {
+    fn read(&mut self) {
+        loop {
+            let mut buf = [0; 2048];
+            match self.socket.read(&mut buf) {
+                Err(e) => {
+                    println!("Error while reading socket: {:?}", e);
+                    return
+                },
+                Ok(len) => {
+                    let mut parser_handler = HttpParser{};
+                    self.http_parser.parse(&mut parser_handler, &buf[0..len]);
+                    if self.http_parser.is_upgrade() {
+                        println!("is_upgrade()");
+                        break;
+                    }
+                }
+            }
+        }
+    }
+
+    fn new(socket: TcpStream) -> WebSocketClient {
+        WebSocketClient {
+            socket: socket,
+            http_parser: Parser::request()
+        }
+    }
+}
+
 struct WebSocketServer {
     socket: TcpListener,
-    clients: HashMap<Token, TcpStream>,
+    clients: HashMap<Token, WebSocketClient>,
     token_counter: usize
 }
 
@@ -42,6 +82,7 @@ fn main() {
         poll.poll(&mut events, None).unwrap();
 
         for event in events.iter() {
+            println!("event: {:?}", event);
             match event.token() {
                 SERVER => {
                     println!("SERVER ready!");
@@ -51,22 +92,17 @@ fn main() {
                     };
                     server.token_counter += 1;
                     let new_token = Token(server.token_counter);
-                    server.clients.insert(new_token, client);
-                    poll.register(&server.clients[&new_token], new_token, Ready::readable(),
-                                  PollOpt::edge() | PollOpt::oneshot()).unwrap();
+                    server.clients.insert(new_token, WebSocketClient::new(client));
+                    poll.register(&server.clients[&new_token].socket, new_token, Ready::readable(),
+                                  PollOpt::edge() /* | PollOpt::oneshot() */).unwrap();
                 },
                 token => {
+                    println!("token: {:?}", token);
                     let mut client = server.clients.get_mut(&token).unwrap();
-                    let mut buf = [0; 2048];
-                    match client.read(&mut buf) {
-                        Err(e) => {
-                            println!("Error while reading socket: {:?}", e);
-                            return
-                        },
-                        Ok(len) => {
-                            println!("client read: {}", String::from_utf8_lossy(&buf[0..len]));
-                        }
-                    }
+                    println!("client get_mut");
+                    client.read();
+//                    poll.register(&client.socket, token, Ready::readable(),
+//                                  PollOpt::edge() | PollOpt::oneshot()).unwrap();
                 }
             }
         }
